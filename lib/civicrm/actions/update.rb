ee58bef87efb0525f826a7d016b81a29bcae6ab5
Peter Woo <peterw@eff.org>
2018-11-02 10:52:40 -0700

    Only send changed attributes in update

--- lib/civicrm/actions/update.rb
+++ lib/civicrm/actions/update.rb
@@ -2,19 +2,24 @@ module CiviCrm
   module Actions
     module Update
       def update(attrs = {})
+        attrs = attrs.reverse_merge(changes.transform_values(&:last))
+
         params = {
           'entity' => self.class.entity_class_name,
           'action' => 'create',
           'id' => id
-        }.merge(attributes).merge(attrs)
+        }.merge(attrs)
 
         response = CiviCrm::Client.request(:post, params)
         refresh_from(response.first.to_hash)
+
+        @previously_changed = changes
+        @changed_attributes.clear
+
+        self
       end
 
       def save
-        @previously_changed = changes
-        @changed_attributes.clear
         update()
       end
     end
