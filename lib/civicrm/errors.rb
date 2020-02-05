af6a31433f56741c9f03467cb4a663d502ce56d2
Alexy Mikhailichenko <alexy@eff.org>
2020-02-05 11:23:35 -0800

    Change custom exceptions to inherit from RuntimeError

--- lib/civicrm/errors.rb
+++ lib/civicrm/errors.rb
@@ -1,9 +1,9 @@
 module CiviCrm
   module Errors
-    class Unauthorized < Exception; end;
-    class InternalError < Exception; end;
-    class Forbidden < Exception; end;
-    class BadRequest < Exception; end;
-    class NotFound < Exception; end;
+    class Unauthorized < RuntimeError; end
+    class InternalError < RuntimeError; end
+    class Forbidden < RuntimeError; end
+    class BadRequest < RuntimeError; end
+    class NotFound < RuntimeError; end
   end
 end
